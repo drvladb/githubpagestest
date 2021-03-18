c6e9fdeb35356ccd28819e22e3f5c8f62a12f22a
Vlad Bondar <vlad@vladb.xyz>
2021-03-18 12:56:08 -0400

    Make change to UI

--- kofta/src/App.js
+++ kofta/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Some new change!</p>
+        <p>Some new change! v2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
